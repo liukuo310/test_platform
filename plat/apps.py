50f7b603493898af2375c05b8a87b80fa1b503b8
liukuo <310980746@qq.com>
2022-09-02 19:12:52 +0800

    配置项目

--- plat/apps.py
+++ plat/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PlatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'plat'
+    verbose_name = '平台应用'
--- test_platform/settings.py
+++ test_platform/settings.py
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -118,7 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
