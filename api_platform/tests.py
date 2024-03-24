ce842dd41be1daef8cfef2ccf2ba7e4629e1618a
liukuo310 <310980746@qq.com>
2024-03-24 13:05:39 +0800

    2024/3/24

--- api_platform/tests.py
+++ api_platform/tests.py
@@ -1,1 +1,16 @@
 from django.test import TestCase
+def world(fn):
+    print('world')
+    return fn()
+
+
+@world
+def hello():
+    print('hello')
+
+
+if __name__ == '__main__':
+    a = 1
+    b = a
+    print(id(a))
+    print(id(b))
--- api_platform/views.py
+++ api_platform/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import View
 
 
 def api_main_view(request):
@@ -20,3 +21,36 @@ def case_manage_view(request):
 def ci_di(request):
     """持续集成界面"""
     return render(request, "api_platform/ci_di.html")
+
+
+class SetApi(View):
+    """接口操作接口"""
+    def put(self, request):
+        api = request.Get("")
+
+    def get(self, request):
+        key = request.GET.get("key")
+        message = dict()
+
+    def post(self, request):
+        body = request.body
+        print(f"传递的参数是{body}")
+
+    def delete(self, request):
+        pass
+
+
+class CaseApi(View):
+    """用例操作接口"""
+
+    def put(self, request):
+        pass
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+    def delete(self, request):
+        pass
